5bad04cb1824716aed64009f02760733a2d5b215
maple3142 <kirby741852963@gmail.com>
2016-12-21 20:48:20 +0800

    complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var low=require('lowdb');
+var db=low('./talk/main.json');
 
 app.engine('ejs', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -17,3 +18,63 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+
+app.get('/',(req,res)=>{
+    res.render('new.ejs');
+});
+app.post('/new',(req,res)=>{
+    var now=Date.now();
+    db.get('list').push({id: now,secret: req.body.secret,auth: req.body.secret!=''}).value();
+    fs.closeSync(fs.openSync('./talk/'+now+'.json', 'w'));//create file
+    res.redirect('../talk/'+now);
+});
+app.get('/talk/:id',(req,res)=>{
+    if(req.session['id'+req.params.id] || !getid(req.params.id).auth){
+        res.render('talk.ejs',{id: req.params.id,msg: (low('./talk/'+req.params.id+'.json').get('msg').value()||[]) });
+    }
+    else{
+        res.render('talkauth.ejs',{id: req.params.id});
+    }
+});
+app.post('/talk/:id/auth',(req,res)=>{
+    var obj=getid(req.params.id);
+    if(obj){
+        if(obj.secret==req.body.secret){
+            req.session['id'+req.params.id]=true;
+        }
+    }
+    res.redirect('/talk/'+req.params.id);
+});
+app.post('/talk/:id/new',(req,res)=>{
+    if(req.session['id'+req.params.id] || !getid(req.params.id).auth){
+        var talkdb=low('./talk/'+req.params.id+'.json');
+        if(!talkdb.get('msg').value())
+            talkdb.set('msg',[]).value();
+        talkdb.get('msg').push({text: req.body.msg,sender: req.connection.remoteAddress}).value();
+    }
+    res.redirect('/talk/'+req.params.id);
+});
+app.get('/talk/:id/delete',(req,res)=>{
+    if(req.session['id'+req.params.id] || !getid(req.params.id).auth){
+        fs.unlink('./talk/'+req.params.id+'.json');
+        var list=db.get('list').value();
+        db.get('list').remove(getid(req.params.id)).value();
+        res.redirect('/');
+    }
+    res.redirect('/talk/'+req.params.id);
+});
+
+
+app.listen(80,()=>console.log('server started'));
+function getid(id){
+    var list=db.get('list').value();
+    var exist=false;
+    var i;
+    for(i in list){
+        if(list[i].id==id){
+            exist=true;
+            break;
+        }
+    }
+    return (exist?list[i]:null);
+}
